c1ed4db010346076c5ae66837d6c1f0e1c9e2faa
lbdm44 <lewis.miller.github@pm.me>
2022-07-03 14:34:49 -0700

    chore: use vitest for testing

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,9 @@
-import test from 'ava'
+import { expect, describe, test } from "vitest";
 
-import { sum } from '../index.js'
+import { sum } from "../index.js";
 
-test('sum from native', (t) => {
-  t.is(sum(1, 2), 3)
-})
+describe("sum", () => {
+  test("sum from native", (t) => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
