bdd6da7deed81ec282e102f5834f58ab277be8d0
lbdm44 <lewis.miller.github@pm.me>
2022-07-04 11:00:49 -0700

    fix: use jest to see if that fixes CI workflow

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -1,9 +1,9 @@
-import { expect, describe, test } from "vitest";
+import { expect, describe, test } from "@jest/globals";
 
 import { sum } from "../index.js";
 
 describe("sum", () => {
-  test("sum from native", (t) => {
+  test("sum from native", () => {
     expect(sum(1, 2)).toBe(3);
   });
 });
